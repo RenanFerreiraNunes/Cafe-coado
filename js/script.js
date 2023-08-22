b82e56a489ff8d4ce5bbb2b9f5e9b0b3ad88ddf7
FredericoVianna <fredyvianna137@gmail.com>
2023-08-21 21:39:10 -0300

    21/08 v5

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 // Selecionar a barra de navegação
 const navbar = document.getElementById("navbar");
+const banner = document.getElementById("banner");
 
 let prevScrollPos = window.pageYOffset;
 let isScrollingUp = false;
 
 // Definir a posição vertical na qual você deseja alterar o estado da barra (em pixels)
-const scrollThreshold = 300;
+const scrollThreshold = 1920;
+
+// Função para comparar a altura da barra de navegação com scrollThreshold
+function compareHeight() {
+  const navbarHeight = navbar.clientHeight;
+
+  if (navbarHeight > scrollThreshold) {
+    console.log('A altura da barra de navegação é maior do que scrollThreshold.');
+    // Faça algo aqui, como adicionar uma classe CSS ou realizar ação específica
+  } else if (navbarHeight < scrollThreshold) {
+    console.log('A altura da barra de navegação é menor do que scrollThreshold.');
+    // Faça algo aqui, como remover uma classe CSS ou realizar ação específica
+  } else {
+    console.log('A altura da barra de navegação é igual a scrollThreshold.');
+    // Faça algo aqui, como ajustar o estilo de acordo com a igualdade
+  }
+}
 
 // Monitorar o evento de rolagem da página
 window.onscroll = function() {
@@ -26,4 +43,7 @@ window.onscroll = function() {
   }
 
   prevScrollPos = currentScrollPos;
+
+  // Comparar a altura da barra de navegação com scrollThreshold
+  compareHeight();
 };
