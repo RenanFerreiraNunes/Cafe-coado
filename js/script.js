c5a88629be3ecf89dad8833ab2f01d10c64dcb4b
FredericoVianna <fredyvianna137@gmail.com>
2023-08-21 21:24:28 -0300

    21/08 v4

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const navbar = document.getElementById("navbar");
 let prevScrollPos = window.pageYOffset;
 let isScrollingUp = false;
 
+// Definir a posição vertical na qual você deseja alterar o estado da barra (em pixels)
+const scrollThreshold = 300;
+
 // Monitorar o evento de rolagem da página
 window.onscroll = function() {
   const currentScrollPos = window.pageYOffset;
@@ -16,7 +19,7 @@ window.onscroll = function() {
   }
 
   // Atualizar a classe CSS com base na direção do scroll
-  if (isScrollingUp) {
+  if (isScrollingUp || currentScrollPos < scrollThreshold) {
     navbar.classList.add("scrolling-up");
   } else {
     navbar.classList.remove("scrolling-up");
