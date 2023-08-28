dec4ee603a8f86933a4d92543072969662d3e478
RenanFerreiraNunes <renan.nunes1904@gmail.com>
2023-08-28 09:02:49 -0300

    28/08 v1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 window.addEventListener('scroll', function(){
-  var nav = document.querySelector("nav");
-  nav.classList.toggle('sticky', window.scrollY > 0);
+  var nav = document.querySelector("header");
+  nav.classList.toggle('sticky', window.scrollY > 600);
 })
