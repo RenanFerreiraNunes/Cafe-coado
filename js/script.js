577ef2347e8929f391c818daa0d9c4a69d035f13
RenanFerreiraNunes <renan.nunes1904@gmail.com>
2023-08-29 16:47:01 -0300

    29/08 v2

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ const produtos = [
       id: 1,
       nome: "Café Espresso",
       descricao: "Um café forte e aromático.",
-      imagens: ["../img/cafeteria-1.png", "img/espresso2.jpg"],
+      imagens: ["./img/cafeteria-1.png", "img/espresso2.jpg"],
       preco: 4.99
     },
     {
       id: 2,
       nome: "Cappuccino",
       descricao: "Uma mistura de café, leite e espuma de leite.",
-      imagens: ["../img/cafeteria-2.png", "img/cappuccino2.jpg"],
+      imagens: ["./img/cafeteria-2.png", "img/cappuccino2.jpg"],
       preco: 5.49
     },
     // Adicione mais produtos aqui...
